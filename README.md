e2a4c7a66ee650ca3bcab2a93c8c3a96980ca6cc
Steve McGrane <smcgrane@optoro.com>
2015-04-07 15:18:53 -0500

    Integrate overmind with new repos

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This cookbook is a skeleton.
 Information on our workflow is available at [here](https://optoro.atlassian.net/wiki/display/DO/Adding+a+cookbook+to+the+Chef+Pipeline)
 
 In order to run guard thta will monitor and test your cookbook against chefspec, rubocop and foodcritic first run bundle install,
-and then run bundle exec guard in a separate console 
+and then run bundle exec guard in a separate console
 
 knife-solo is used to create test databags for use with serverspec:
 ```
--- sanitize_repo.sh
+++ sanitize_repo.sh
@@ -7,7 +7,7 @@ DIRNAME=${PWD##*/}
 for i in $(grep -rl optoro_skel * | grep -v .git | grep -v ${0##*/} ); do
   echo "Replacing optoro_skel in ${i}"
   #sed -i -e "s/optoro_skel/$DIRNAME/g" "$i"
-  ruby -pi -e "gsub('optoro_skel', '$DIRNAME')" $i               
+  ruby -pi -e "gsub('optoro_skel', '$DIRNAME')" $i
 done
 
 # Remove this file from the new repo
@@ -16,5 +16,11 @@ git rm -f $0
 # remove the optoro_skel git commit history and fix the git remote
 rm -rf ./.git
 git init
+bundle install
+berks install
+overcommit --sign pre-commit
+overcommit --install
 git remote add origin git@github.com:optoro-devops/$DIRNAME
-
+git add .
+git commit -am "First Commit"
+git push origin master
